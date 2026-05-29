8eed353c5e47d5638806d04e2d11cefc42ee0dcc
disfigure3211-jpg <your.email@example.com>
2026-05-30 04:46:16 +0800

    add backend for vercel

--- porras-server/index.js
+++ porras-server/index.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
